d4dff2268f8c312bdebc13a7235a4eb7bf747b6f
Guilene01 <guileneerna@yahoo.fr>
2025-10-03 18:58:36 -0400

    newcode

---
+++ terraform/alb.tf
@@ -0,0 +1,66 @@
+// target group
+
+resource "aws_lb_target_group" "tg1" {
+  name        = "ntc-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "instance"
+  vpc_id      = aws_vpc.my-vpc.id
+
+  health_check {
+    enabled             = true
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = 200
+    interval            = 10
+    timeout             = 6
+    port                = "traffic-port"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name = "app-tg"
+    Env  = "dev"
+  }
+  depends_on = [aws_vpc.my-vpc]
+}
+
+//attach instances to target group
+resource "aws_lb_target_group_attachment" "ntc-tg-attach1" {
+  target_group_arn = aws_lb_target_group.tg1.arn
+  target_id        = aws_instance.web1.id
+  port             = 80
+
+}
+resource "aws_lb_target_group_attachment" "ntc-tg-attach2" {
+  target_group_arn = aws_lb_target_group.tg1.arn
+  target_id        = aws_instance.web2.id
+  port             = 80
+
+}
+
+// application load balancer
+resource "aws_lb" "alb" {
+  name                       = "ntc-alb"
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.alb_sg.id]
+  subnets                    = [aws_subnet.public1.id, aws_subnet.public2.id]
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "ntc-alb"
+    Env  = "dev"
+  }
+}
+// listener for alb
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg1.arn
+  }
+}
