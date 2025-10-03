bf5279f467bbfa1f0914182101679d0b02d3ddb8
Guilene01 <guileneerna@yahoo.fr>
2025-10-03 19:06:18 -0400

    newcode

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket       = "gt-bucket-terraform"
     key          = "week10/terraform.tfstate"
     region       = "us-east-1"
-    use_lockfile = false
+    #use_lockfile = false
   }
 
 
