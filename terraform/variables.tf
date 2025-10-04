3f3c29324d1d3dea06c67fb37d980e2d632faaff
Guilene01 <guileneerna@yahoo.fr>
2025-10-04 01:05:20 -0400

    newcode

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,8 +4,8 @@ variable "environment" {
   default     = "dev"
 }
 
-variable "enable_deletion_protection" {
-  description = "Enable deletion protection on resources"
-  type        = bool
-  default     = false # Set to false for CI/CD
-}
+# variable "enable_deletion_protection" {
+#   description = "Enable deletion protection on resources"
+#   type        = bool
+#   default     = false # Set to false for CI/CD
+# }
